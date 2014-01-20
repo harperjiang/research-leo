98563ca9f47c91d8f9ed20d1f878a84864479c83
Harper <harperjiang@msn.com>
2014-01-19 22:18:53 -0500

    Positive Sample

--- src/edu/clarkson/cs/wpcomp/img/transform/ImageTransformMain.java
+++ src/edu/clarkson/cs/wpcomp/img/transform/ImageTransformMain.java
@@ -6,36 +6,18 @@ import java.io.File;
 
 import javax.imageio.ImageIO;
 
-
 public class ImageTransformMain {
 
 	public static void main(String[] args) throws Exception {
 		// Read image
-		BufferedImage image = ImageIO.read(new File("res/image/ebay_1.jpg"));
-
-		BufferedImage[] variants = new BufferedImage[8];
-		variants[0] = ImageTransformer.transform(
-				image,
-				ImageTransformer.centerScale(image.getWidth() / 2,
-						image.getHeight() / 2, 1.25));
-		variants[1] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(-10, 0));
-		variants[2] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(-10, 10));
-		variants[3] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(0, 10));
-		variants[4] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(10, 10));
-		variants[5] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(10, 0));
-		variants[6] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(10, -10));
-		variants[7] = ImageTransformer.transform(image,
-				AffineTransform.getTranslateInstance(0, -10));
-
-		for (int i = 0; i < variants.length; i++) {
-			ImageIO.write(variants[i], "png", new File(
-					"res/image/ebay_1_output_" + i + ".png"));
+		File folder = new File("/home/harper/Downloads/negativeImages");
+		for (File file : folder.listFiles()) {
+			BufferedImage image = ImageIO.read(file);
+			BufferedImage output = ImageTransformer.transform(image,
+					AffineTransform.getScaleInstance(500d / image.getWidth(),
+							500d / image.getHeight()));
+			ImageIO.write(output, "jpg",
+					new File("res/image/negative/" + file.getName()));
 		}
 	}
 }
